9eb01049976119538e0f67b581784f968f0a2449
yh_n <2424313815@qq.com>
2024-07-13 17:00:37 +0800

    Initial commit

---
+++ maven-javaee-project-02/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,12 @@
+package com.atguigu;
+
+/**
+ * @author 小牛
+ * @version 1.0
+ * @date 2024/7/12 15:50
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
